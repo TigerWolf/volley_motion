a9e8fc14234cc325ce5e556a87c5285cfb88dce1
TigerWolf <hiddentiger@gmail.com>
2015-01-31 16:26:02 +1030

    fix issue with error listener

--- lib/volley_motion/error_listener.rb
+++ lib/volley_motion/error_listener.rb
@@ -1,5 +1,9 @@
 module VolleyMotion
   class ErrorListener
+
+    def initialize
+
+    end
     def onErrorResponse(error)
       if error.toString == "com.android.volley.AuthFailureError"
         p "Authentication error"
