aa8ecb9f76b29830ff801e8ad544fd9484a7b83d
TigerWolf <hiddentiger@gmail.com>
2015-04-13 21:48:40 +0930

    Add new error cases

--- lib/volley_motion/error_listener.rb
+++ lib/volley_motion/error_listener.rb
@@ -9,11 +9,22 @@ module VolleyMotion
 
     def onErrorResponse(error)
       error_result = { :error => :network_error, :message => "Network error."}
+      p error.class.toString
       case error.class.toString
       when "class com.android.volley.AuthFailureError"
         error_result = { :error => :auth_failure, :message => "Authentication Failure."}
+      when "class com.android.volley.NetworkError"
+        error_result = { :error => :auth_failure, :message => "Network error."}
       when "class com.android.volley.NoConnectionError"
         error_result = { :error => :no_connection, :message => "No connection."}
+      when "class com.android.volley.ParseError"
+        error_result = { :error => :no_connection, :message => "Invalid server response."}
+      when "class com.android.volley.TimeoutError"
+        error_result = { :error => :timeout, :message => "Connection timed out."}
+      when "class com.android.volley.ServerError"
+        error_result = { :error => :timeout, :message => "Server error."}
+      when "class com.android.volley.VolleyError"
+        error_result = { :error => :volley, :message => "Network error."}
       end
       @activity.error_response(error_result)
     end
